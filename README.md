30b903ea4c54114fc39e51d87134c3c02a858750
JasonColeyNZ <jason@software-solutions.co.nz>
2022-08-21 11:48:43 +1000

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-hxzp3y
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-hxzp3y)
---
+++ src/app/app.component.ts
@@ -0,0 +1,35 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  itemImageUrl = '../assets/phone.png';
+  isUnchanged = false;
+  classes = 'special';
+  parentItem = 'lamp';
+
+  currentItems = [
+    {
+      id: 21,
+      name: 'phone',
+    },
+    {
+      id: 15,
+      name: 'lamp',
+    },
+  ];
+
+  interpolationTitle = 'Interpolation';
+  propertyTitle = 'Property binding';
+
+  evilTitle = 'Template <script>alert("evil never sleeps")</script> Syntax';
+}
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
